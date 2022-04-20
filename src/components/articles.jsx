905d4b1b3aa2210be57664d344084663efd6020d
kawla95 <kawlamansur@hotmail.co.uk>
2022-04-20 22:54:13 +0100

    added a query to sort articles by topic

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -4,24 +4,52 @@ import { getArticles } from "../utils/api-endpoints";
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
+  const [topic, setTopic] = useState("");
+  const [sort_by, setSort_by] = useState("");
+  const [order, setOrder] = useState("");
 
   useEffect(() => {
-    getArticles().then((res) => {
+    getArticles(topic, sort_by, order).then((res) => {
       setArticles(res);
       console.log(res);
     });
-  }, []);
+  }, [topic, sort_by, order]);
 
   return (
     <div>
+      <button onClick={() => setTopic("")}>All Topics</button>
+      <button onClick={() => setTopic("coding")}>Coding</button>
+      <button onClick={() => setTopic("football")}>Football</button>
+      <button onClick={() => setTopic("cooking")}>Cooking</button>
+      <label for="sort_by">Sort By</label>
+      <select name="sort_by" id="sort_by">
+        <option
+          value="comment_count"
+          onClick={() => setSort_by("comment_count")}
+        >
+          Comment Count
+        </option>
+        <option value="created_at" onClick={() => setSort_by("created_at")}>
+          Date
+        </option>
+        <option value="votes" onClick={() => setSort_by("votes")}>
+          Votes
+        </option>
+      </select>
+      <button onClick={() => setOrder("ASC")}>Latest</button>
+      <button onClick={() => setOrder("DESC")}>Oldest</button>
       <ul>
         {articles.map((article) => {
           return (
             <>
               <li key={article.article_id}>
                 <h3>{article.title}</h3>
+                <p>{article.topic}</p>
                 <h4>{article.author}</h4>
                 <p>{article.body}</p>
+                <p>Votes: {article.votes}</p>
+                <p>Created at: {article.created_at}</p>
+                <p>Comments: {article.comment_count}</p>
               </li>
             </>
           );
--- src/utils/api-endpoints.js
+++ src/utils/api-endpoints.js
@@ -4,12 +4,12 @@ const articlesApi = axios.create({
   baseURL: "https://backend-of-news-app.herokuapp.com/api",
 });
 
-export const getArticles = (user_topic, user_sort_by) => {
-  return articlesApi
-    .get("/articles", {
-      params: { topic: user_topic, sort_by: user_sort_by },
-    })
-    .then((res) => {
-      return res.data.articles;
-    });
+export const getArticles = (user_topic, user_sort_by, user_order) => {
+  let path = "/articles?";
+  if (user_topic) path += `topic=${user_topic}`;
+  if (user_sort_by) path += `&&sort_by=${user_sort_by}`;
+  user_order ? (path += `&&order=${user_order}`) : (path += `&&order=DESC`);
+  return articlesApi.get(path).then((res) => {
+    return res.data.articles;
+  });
 };
