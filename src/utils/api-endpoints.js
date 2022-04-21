95ac8c85091747c893bec022d6ea8ff1b4019558
kawla95 <kawlamansur@hotmail.co.uk>
2022-04-21 11:50:48 +0100

    taken off sort by queries

--- src/utils/api-endpoints.js
+++ src/utils/api-endpoints.js
@@ -4,12 +4,8 @@ const articlesApi = axios.create({
   baseURL: "https://backend-of-news-app.herokuapp.com/api",
 });
 
-export const getArticles = (user_topic, user_sort_by) => {
-  return articlesApi
-    .get("/articles", {
-      params: { topic: user_topic, sort_by: user_sort_by },
-    })
-    .then((res) => {
-      return res.data.articles;
-    });
+export const getArticles = () => {
+  return articlesApi.get("/articles").then((res) => {
+    return res.data.articles;
+  });
 };
