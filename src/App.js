06ca768a10f86a4d952f99677456fc636b468106
kawla95 <kawlamansur@hotmail.co.uk>
2022-06-08 17:46:53 +0100

    deleted browserrouter from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Articles from "./components/Articles";
 import Article from "./components/Article";
 import "bootstrap/dist/css/bootstrap.min.css";
